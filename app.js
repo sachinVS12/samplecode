1ac0e54fa5c933d9dc55da0513c03db3fdc63ba1
Sachin <vs1sachi12@gmail.com>
2025-07-26 17:15:21 +0530

    first commit9

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoosee");
+const bcrypt = require("bcrypt");
+const app = express();
+
+
+app.post('/signup', async(req, res)=>{
+    try{
+    const {username, password} = req.body;
+    const hash = await bcrypt.hash(password, 10);
+    await User.create({username, password: hash});
+    res.send('signup succesfull');
+}catch(error){
+
+}});
+
